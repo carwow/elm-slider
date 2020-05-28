19e0b7325d6943a44ac562f1f592e5f92e3c9de5
Vincent Navetat <vincent.navetat@carwow.co.uk>
2020-05-28 13:47:41 +0100

    Refactor

--- src/RangeSlider.elm
+++ src/RangeSlider.elm
@@ -27,27 +27,16 @@ onChange msg input =
     Html.Events.on "change" (Json.Decode.map msg input)
 
 
-sliderInputView : CommonAttributes -> ValueAttributes msg -> Json.Decode.Decoder Float -> Maybe String -> Html msg
-sliderInputView commonAttributes valueAttributes input extraClass =
-    let
-        baseClass =
-            "input-range"
-
-        class =
-            case extraClass of
-                Just s ->
-                    baseClass ++ " " ++ s
-
-                Nothing ->
-                    baseClass
-    in
+sliderInputView : CommonAttributes -> ValueAttributes msg -> Json.Decode.Decoder Float -> List String -> Html msg
+sliderInputView commonAttributes valueAttributes input extraClasses =
     Html.input
         [ Html.Attributes.type_ "range"
         , Html.Attributes.min <| String.fromFloat commonAttributes.min
         , Html.Attributes.max <| String.fromFloat commonAttributes.max
         , Html.Attributes.step <| String.fromFloat commonAttributes.step
         , Html.Attributes.value <| String.fromFloat valueAttributes.value
-        , Html.Attributes.class class
+        , Html.Attributes.class "input-range"
+        , Html.Attributes.classList <| List.map (\c -> ( c, True )) extraClasses
         , onChange valueAttributes.change input
         ]
         []
